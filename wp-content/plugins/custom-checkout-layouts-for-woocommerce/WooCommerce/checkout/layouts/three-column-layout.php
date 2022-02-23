5bfffdc899ff46e5cb83c6fe4c4f745f49f194a4
taitamfc <taitam8590@gmail.com>
2022-02-22 21:42:02 -0800

    thanh toan

--- wp-content/plugins/custom-checkout-layouts-for-woocommerce/WooCommerce/checkout/layouts/three-column-layout.php
+++ wp-content/plugins/custom-checkout-layouts-for-woocommerce/WooCommerce/checkout/layouts/three-column-layout.php
@@ -1,19 +1,69 @@
-	<div class="grid-col-1 grid-col-checkout">
-	
-	<h3 class="border_html"> <?php esc_html_e('Billing details', 'woocommerce' ); ?></h3>
-	<?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_billing_details_section.php';	?>
-	   
-	</div>
-	
-	<div class="grid-col-2 grid-col-checkout">
-		 <h3 class="border_html"><?php esc_html_e('Review your orders', 'woocommerce' ); ?></h3>
-	 <?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_review_order_section.php'; ?> 
-		
-	</div>
-	<div class="grid-col-3 grid-col-checkout">
-	
-	<h3 class="border_html"><?php esc_html_e('Payments', 'woocommerce' ); ?></h3>
-	<?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_payment_section.php'; ?>
-	   
-	</div>
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>	
+<div class="tabbed-content process-item">
+    <ul class="nav nav-uppercase nav-size-normal nav-center">
+        <li class="tab has-icon active">
+            <a href="#tab_tab-1-title">
+                <div class="icon">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart" role="img"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+                        class="svg-inline--fa fa-shopping-cart fa-w-18">
+                        <path fill="currentColor"
+                            d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
+                            class=""></path>
+                    </svg>
+                </div>
+                <span>Chọn sản phẩm</span>
+            </a>
+        </li>
+        <li class="tab has-icon">
+            <a href="#tab_tab-2-title">
+                <div class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="address-card"
+                        role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+                        class="svg-inline--fa fa-address-card fa-w-18">
+                        <path fill="currentColor"
+                            d="M528 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-352 96c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H86.4C74 384 64 375.4 64 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2zM512 312c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16z"
+                            class=""></path>
+                    </svg>
+                </div>
+                <span>Thông tin đặt hàng</span>
+            </a>
+        </li>
+        <li class="tab has-icon">
+            <a href="#tab_tab-3-title">
+                <div class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="credit-card"
+                        role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+                        class="svg-inline--fa fa-credit-card fa-w-18">
+                        <path fill="currentColor"
+                            d="M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z"
+                            class=""></path>
+                    </svg>
+                </div>
+                <span>Thanh toán</span>
+            </a>
+        </li>
+        <li class="tab has-icon">
+			<a href="#tab_tab-4-title">
+                <div data-v-76247bda="" class="icon"><svg data-v-76247bda="" aria-hidden="true" focusable="false"
+                        data-prefix="fas" data-icon="box-open" role="img" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 640 512" class="svg-inline--fa fa-box-open fa-w-20">
+                        <path data-v-76247bda="" fill="currentColor"
+                            d="M425.7 256c-16.9 0-32.8-9-41.4-23.4L320 126l-64.2 106.6c-8.7 14.5-24.6 23.5-41.5 23.5-4.5 0-9-.6-13.3-1.9L64 215v178c0 14.7 10 27.5 24.2 31l216.2 54.1c10.2 2.5 20.9 2.5 31 0L551.8 424c14.2-3.6 24.2-16.4 24.2-31V215l-137 39.1c-4.3 1.3-8.8 1.9-13.3 1.9zm212.6-112.2L586.8 41c-3.1-6.2-9.8-9.8-16.7-8.9L320 64l91.7 152.1c3.8 6.3 11.4 9.3 18.5 7.3l197.9-56.5c9.9-2.9 14.7-13.9 10.2-23.1zM53.2 41L1.7 143.8c-4.6 9.2.3 20.2 10.1 23l197.9 56.5c7.1 2 14.7-1 18.5-7.3L320 64 69.8 32.1c-6.9-.8-13.5 2.7-16.6 8.9z"
+                            class=""></path>
+                    </svg></div>
+                <span>Hoàn tất đặt hàng</span>
+            </a>
+		</li>
+    </ul>
+    <div class="tab-panels">
+        <div class="panel entry-content active" id="tab_tab-1-title">
+            <?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_review_order_section.php'; ?>
+        </div>
+        <div class="panel entry-content" id="tab_tab-2-title">
+            <?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_billing_details_section.php';	?>
+        </div>
+        <div class="panel entry-content" id="tab_tab-3-title">
+            <?php include_once CCLW_PLUGIN_DIR . 'WooCommerce/checkout/cclw_includes/cclw_payment_section.php'; ?>
+        </div>
+    </div>
+</div>
+
+<?php //do_action( 'woocommerce_checkout_after_order_review' ); ?>
